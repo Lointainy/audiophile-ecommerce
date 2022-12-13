9a54820a776abcb7ddab766ca56226d7140c527e
Eugene_dev <lointainyyy@gmail.com>
2022-12-13 12:09:41 +0200

    add basic styles

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,12 @@ import App from './App'
 /* Styles */
 import '@styles/main.scss'
 
+/* Fonts */
+import '@fontsource/manrope'
+import '@fontsource/manrope/400.css'
+import '@fontsource/manrope/500.css'
+import '@fontsource/manrope/700.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
