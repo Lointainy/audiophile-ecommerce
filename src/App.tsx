8dccc68b0c5b0240596766f560708c8c84bd70f2
Eugene_dev <lointainyyy@gmail.com>
2022-12-13 17:47:47 +0200

    add logic for theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@
 import router from '@router/router'
 import { RouterProvider } from 'react-router-dom'
 
+/* Hooks */
+import { useTheme } from '@hooks/useTheme'
+
 const App: React.FC = () => {
+  const { userTheme, toggleTheme } = useTheme()
+
+  console.log(userTheme)
   return (
-    <>
+    <div className={`app ${userTheme}`}>
       <RouterProvider router={router}></RouterProvider>
-    </>
+    </div>
   )
 }
 
