863e18572bfc2eb67b78cf8f39d7cb8b92def891
Eugene_dev <lointainyyy@gmail.com>
2022-12-13 17:35:35 +0200

    fix logic

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,8 +2,9 @@ import { createBrowserRouter } from 'react-router-dom'
 import { ROUTES } from './routes'
 
 /* Pages */
+import CategoryPage from '@/pages/CategoryPage/CategoryPage'
 import Layout from '@layout/Layout'
-import { CheckoutPage, HomePage, ProductPage } from '@pages'
+import { CheckoutPage, HomePage, NotFoundPage, ProductPage } from '@pages'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,8 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       { path: ROUTES.checkout, element: <CheckoutPage /> },
       { path: ROUTES.product(), element: <ProductPage /> },
+      { path: ROUTES.category(), element: <CategoryPage /> },
+      { path: ROUTES.error, element: <NotFoundPage /> },
     ],
   },
 ])
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,7 @@
 export const ROUTES = {
   home: '/',
-  category: (name: string = '') => (name ? `/category/${name}` : '/category/:productName'),
+  category: (name: string = '') => (name ? `/category/${name}` : '/category/:categoryName'),
   product: (id: number = 0) => (id ? `/product/${id}` : '/product/:productId'),
   checkout: '/checkout',
+  error: '*',
 }
