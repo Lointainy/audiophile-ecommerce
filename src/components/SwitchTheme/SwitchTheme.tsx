c43afd64889c47b5dd739ab426347cb8f03543c7
Eugene_dev <lointainyyy@gmail.com>
2022-12-13 19:11:08 +0200

    fix logic, add style

--- src/components/SwitchTheme/SwitchTheme.tsx
+++ src/components/SwitchTheme/SwitchTheme.tsx
@@ -1,13 +1,29 @@
+import { useEffect } from 'react'
+
 /* Hooks */
 import { useTheme } from '@hooks/useTheme'
+
+/* Store */
+import { useAppDispatch } from '@hooks/useRedux'
+import { SET_THEME } from '@store/reducers/dataReducer'
+
+/* Style */
+import style from './SwitchTheme.module.scss'
 
 /* Icons */
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 
 const SwitchTheme: React.FC = () => {
-  const { value, toggleTheme } = useTheme()
+  const { value, toggleTheme, userTheme } = useTheme()
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(SET_THEME(userTheme))
+  }, [value])
+
   return (
-    <div className="theme__icon" onClick={toggleTheme}>
+    <div className={style.theme__icon} onClick={toggleTheme}>
       {value ? <Icon icon="sun" /> : <Icon icon="moon" />}
     </div>
   )
