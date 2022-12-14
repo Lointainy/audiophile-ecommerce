5e332d6d437e4e46615be9d00aa619a8b1b1bc56
Eugene_dev <lointainyyy@gmail.com>
2022-12-14 19:34:42 +0200

    add toggleMenu button

---
+++ src/components/ToggleMenu/ToggleMenu.tsx
@@ -0,0 +1,25 @@
+/* Store */
+import { useAppDispatch } from '@hooks/useRedux'
+import { TOGGLE_NAV } from '@store/reducers/dataReducer'
+
+/* Styles */
+import style from './ToggleMenu.module.scss'
+
+/* Icons */
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+
+const ToggleMenu: React.FC = () => {
+  const dispatch = useAppDispatch()
+
+  const handleDropdownMenu = () => {
+    dispatch(TOGGLE_NAV)
+  }
+
+  return (
+    <div className={style.toggle__icon} onClick={handleDropdownMenu}>
+      <Icon icon="bars" />
+    </div>
+  )
+}
+
+export default ToggleMenu
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as MobileNav } from './MobileNav/MobileNav'
 export { default as Overlay } from './Overlay/Overlay'
 export { default as SwitchTheme } from './SwitchTheme/SwitchTheme'
+export { default as ToggleMenu } from './ToggleMenu/ToggleMenu'
