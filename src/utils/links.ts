ad01bfc4e28f7f3d628bfaddb3d673949d39c5cc
Eugene_dev <lointainyyy@gmail.com>
2022-12-15 15:38:39 +0200

    add utils links

---
+++ src/utils/links.ts
@@ -0,0 +1,25 @@
+export const links = [
+  {
+    id: 1,
+    title: 'home',
+    url: '/',
+  },
+  {
+    id: 2,
+    title: 'Headphones',
+    url: 'headphones',
+    img: '/img/shared/desktop/image-category-thumbnail-headphones.png',
+  },
+  {
+    id: 3,
+    title: 'Speakers',
+    url: 'speakers',
+    img: '/img/shared/desktop/image-category-thumbnail-speakers.png',
+  },
+  {
+    id: 4,
+    title: 'Earphones',
+    url: 'earphones',
+    img: '/img/shared/desktop/image-category-thumbnail-earphones.png',
+  },
+]
