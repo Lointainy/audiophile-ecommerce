0ce7fd29db1f0911b9dbabcde4c527985cbf8bee
Eugene_dev <lointainyyy@gmail.com>
2022-12-19 13:17:58 +0200

    remove logic

--- src/components/CartToggle/CartToggle.tsx
+++ src/components/CartToggle/CartToggle.tsx
@@ -1,6 +1,5 @@
 /* Store */
 import { useAppDispatch } from '@/hooks/useRedux'
-import { SET_OVERLAY, TOGGLE_MODAL_CART } from '@store/reducers/dataReducer'
 
 /* Style */
 import style from './CartToggle.module.scss'
@@ -11,10 +10,7 @@ import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 const CartToggle: React.FC = () => {
   const dispatch = useAppDispatch()
 
-  const handleToggleCart = () => {
-    dispatch(TOGGLE_MODAL_CART())
-    dispatch(SET_OVERLAY())
-  }
+  const handleToggleCart = () => {}
   return (
     <div className={style.cart__icon} onClick={handleToggleCart}>
       <Icon icon="cart-shopping" />
