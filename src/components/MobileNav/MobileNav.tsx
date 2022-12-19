144dd628fb55199e469b56c29eb2076502b6bdca
Eugene_dev <lointainyyy@gmail.com>
2022-12-19 21:28:49 +0200

    fix logic

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -2,6 +2,10 @@
 import { ROUTES } from '@router/routes'
 import { NavLink } from 'react-router-dom'
 
+/* Store */
+import { useAppDispatch } from '@hooks/useRedux'
+import { TOGGLE_NAV } from '@store/reducers/dataReducer'
+
 /* Utils */
 import { links } from '@/utils/links'
 
@@ -12,6 +16,8 @@ import style from './MobileNav.module.scss'
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 
 const MobileNav: React.FC = () => {
+  const dispatch = useAppDispatch()
+
   const category = links.filter((i) => i.id != 1)
 
   return (
@@ -20,7 +26,10 @@ const MobileNav: React.FC = () => {
         {category.map((item) => {
           return (
             <li key={item.id} className={style.item}>
-              <NavLink to={ROUTES.category(`${item.url}`)} className={style.item__field}>
+              <NavLink
+                to={ROUTES.category(`${item.url}`)}
+                className={style.item__field}
+                onClick={() => dispatch(TOGGLE_NAV())}>
                 <img src={`${item.img}`} alt="" className={style.img} />
                 <div className={style.title}>{item.title}</div>
                 <div className={style.subtitle}>
