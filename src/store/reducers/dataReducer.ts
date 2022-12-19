bab5efa05b8fcba88bd9a17df5ff20dd2ce3f506
Eugene_dev <lointainyyy@gmail.com>
2022-12-19 13:17:48 +0200

    remove logic

--- src/store/reducers/dataReducer.ts
+++ src/store/reducers/dataReducer.ts
@@ -3,9 +3,7 @@ import { stat } from 'fs'
 
 const initialState = {
   colorTheme: 'light',
-  overlay: false,
   nav: false,
-  cartModal: false,
 }
 
 export const dataSlice = createSlice({
@@ -15,18 +13,12 @@ export const dataSlice = createSlice({
     SET_THEME: (state, action) => {
       state.colorTheme = action.payload
     },
-    SET_OVERLAY: (state) => {
-      state.overlay = !state.overlay
-    },
     TOGGLE_NAV: (state) => {
       state.nav = !state.nav
-    },
-    TOGGLE_MODAL_CART: (state) => {
-      state.cartModal = !state.cartModal
     },
   },
 })
 
-export const { SET_THEME, SET_OVERLAY, TOGGLE_NAV, TOGGLE_MODAL_CART } = dataSlice.actions
+export const { SET_THEME, TOGGLE_NAV } = dataSlice.actions
 
 export default dataSlice.reducer
