eb24a450479f572524c948d752f2ca09daf278f0
Eugene_dev <lointainyyy@gmail.com>
2022-12-20 14:20:13 +0200

    fix header styles

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -16,53 +16,33 @@ const Header: React.FC = () => {
 
   return (
     <header>
-      <div className={style.logo}>
-        <NavLink to={ROUTES.home}>
-          <img src="" alt="" />
-          logo
-        </NavLink>
-      </div>
-
-      <div className={style.mobile}>
-        <div className={style.toggle}>
-          <ToggleMenu />
+      <div className={style.field}>
+        <div className={style.logo}>
+          <NavLink to={ROUTES.home}>
+            <img src="" alt="" />
+            logo
+          </NavLink>
         </div>
-        {mobileNav && <MobileNav />}
-      </div>
 
-      <div className={style.desktop}>
-        <DesktopNav />
-      </div>
+        <div className={style.mobile}>
+          <div className={style.toggle}>
+            <ToggleMenu />
+          </div>
+          {mobileNav && <MobileNav />}
+        </div>
 
-      <div className={style.theme}>
-        <SwitchTheme />
-      </div>
+        <div className={style.desktop}>
+          <DesktopNav />
+        </div>
 
-      <div className={style.cart}>
-        <CartToggle />
-      </div>
-      {/* <div className={style.mobile}>
-        <ToggleMenu />
-      </div>
+        <div className={style.theme}>
+          <SwitchTheme />
+        </div>
 
-      <div className={style.logo}>
-        <NavLink to={ROUTES.home}>
-          <img src="" alt="" />
-          logo
-        </NavLink>
+        <div className={style.cart}>
+          <CartToggle />
+        </div>
       </div>
-
-      <div className={style.desktop}>
-        <DesctopNav />
-      </div>
-
-      <div className={style.theme}>
-        <SwitchTheme />
-      </div>
-
-      <div className={style.cart}>
-        <CartToggle />
-      </div> */}
     </header>
   )
 }
