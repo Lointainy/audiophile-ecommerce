74b58ccab67f1d918f7aca475e394ae27b8337a5
Eugene_dev <lointainyyy@gmail.com>
2023-01-03 12:28:07 +0200

    add logic for get data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useEffect } from 'react'
+
 /* Router */
 import router from '@router/router'
 import { RouterProvider } from 'react-router-dom'
 
-/* Hooks */
-import { useAppSelector } from '@hooks/useRedux'
+/* Store */
+import { useAppDispatch, useAppSelector } from '@hooks/useRedux'
+import { getProducts } from '@store/reducers/dataSlice'
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch()
+
   const theme = useAppSelector((store) => store.ui.colorTheme)
+
+  useEffect(() => {
+    dispatch(getProducts())
+  }, [])
 
   return (
     <div className={`app ${theme}`}>
