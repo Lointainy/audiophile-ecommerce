806d400e6960041c3399439393a0a199de74eb19
Eugene_dev <lointainyyy@gmail.com>
2023-01-03 12:29:56 +0200

    added logic to get data

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
+
+/* Store */
+import { useAppDispatch } from '@hooks/useRedux'
+import { getGalleryProducts, getNewProduct } from '@store/reducers/dataSlice'
+
 /* Components */
 import { CategoryList, HeaderContent } from '@components'
 
 const HomePage: React.FC = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getNewProduct())
+    dispatch(getGalleryProducts())
+  }, [])
   return (
     <>
       <HeaderContent />
