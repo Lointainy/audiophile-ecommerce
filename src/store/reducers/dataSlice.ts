8194c9cdcf0d6f2b19e112cbf7136c9c427bc0c9
Eugene_dev <lointainyyy@gmail.com>
2023-01-03 12:27:48 +0200

    added new actions to get data

--- src/store/reducers/dataSlice.ts
+++ src/store/reducers/dataSlice.ts
@@ -1,14 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 const initialState = {
-  products: '',
+  products: [],
   product: '',
+  newProduct: {},
+  galleryProducts: [],
 }
+
+export const getProducts = createAsyncThunk('products/getProducts', async () => {
+  const { data } = await axios.get('http://localhost:3000/api/products')
+  return data
+})
+
+export const getNewProduct = createAsyncThunk('products/getNewProduct', async () => {
+  const { data } = await axios.get('http://localhost:3000/api/new')
+  return data
+})
+
+export const getGalleryProducts = createAsyncThunk('products/getGalleryProducts', async () => {
+  const { data } = await axios.get('http://localhost:3000/api/gallery')
+  return data
+})
 
 export const dataSlice = createSlice({
   name: 'data',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.products = []
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.products = action.payload
+        console.log(state.products)
+      })
+    builder
+      .addCase(getNewProduct.pending, (state) => {
+        state.product = {}
+      })
+      .addCase(getNewProduct.fulfilled, (state, action) => {
+        state.product = action.payload
+        console.log(state.product)
+      })
+    builder
+      .addCase(getGalleryProducts.pending, (state) => {
+        state.galleryProducts = {}
+      })
+      .addCase(getGalleryProducts.fulfilled, (state, action) => {
+        state.galleryProducts = action.payload
+        console.log(state.galleryProducts)
+      })
+  },
 })
 
 export const {} = dataSlice.actions
