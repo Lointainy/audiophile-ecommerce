2c172a8978e96bcef16a448c981cb890b22e13a6
Eugene_dev <lointainyyy@gmail.com>
2023-01-24 15:02:20 +0200

    added styles

--- src/components/OtherInfo/OtherInfo.tsx
+++ src/components/OtherInfo/OtherInfo.tsx
@@ -4,20 +4,25 @@ import style from './OtherInfo.module.scss'
 const OtherInfo: React.FC = ({ features, includes }) => {
   return (
     <div className={style.info}>
-      <h2 className={style.title}>Features</h2>
-      <p className={style.features}>{features}</p>
-
-      <h2 className={style.title}>in the box</h2>
-      <ul className={style.includes}>
-        {includes?.map((i) => {
-          return (
-            <li key={i.item} className={style.item}>
-              <span className={style.quantity}>x{i.item}</span>
-            </li>
-          )
-        })}
-      </ul>
+      <div className={style.features}>
+        <h2 className={style.title}>Features</h2>
+        <p className={style.desc}>{features}</p>
+      </div>
+      <div className={style.includes}>
+        <h2 className={style.title}>in the box</h2>
+        <ul className={style.list}>
+          {includes?.map((i) => {
+            return (
+              <li key={i.item} className={style.item}>
+                <span className={style.accent}>{i.quantity}x</span>
+                <p className={style.include}>{i.item}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
+
 export default OtherInfo
