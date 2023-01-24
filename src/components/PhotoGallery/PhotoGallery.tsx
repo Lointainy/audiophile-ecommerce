95a41e791117090a2eb3bbde87f92fa84b5d3844
Eugene_dev <lointainyyy@gmail.com>
2023-01-24 17:53:45 +0200

    addedd styles for photo gallery component

--- src/components/PhotoGallery/PhotoGallery.tsx
+++ src/components/PhotoGallery/PhotoGallery.tsx
@@ -1,22 +1,29 @@
+/* Hooks */
+import { useMediaQuery } from '@hooks/useMediaQuery'
+
 /* Styles */
 import style from './PhotoGallery.module.scss'
 
 const PhotoGallery: React.FC = ({ gallery }) => {
+  const {
+    mediaQuery: { name: picSize },
+  } = useMediaQuery()
+
   return (
     <ul className={style.gallery}>
       <li className={style.field}>
         <div className={style.img}>
-          <img src="" alt="" />
+          <img src={`../${gallery?.first[picSize]}`} alt="" />
         </div>
       </li>
       <li className={style.field}>
         <div className={style.img}>
-          <img src="" alt="" />
+          <img src={`../${gallery?.second[picSize]}`} alt="" />
         </div>
       </li>
       <li className={style.field}>
         <div className={style.img}>
-          <img src="" alt="" />
+          <img src={`../${gallery?.third[picSize]}`} alt="" />
         </div>
       </li>
     </ul>
