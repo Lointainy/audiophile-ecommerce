7f82753540763a5b4284e35d75a416da6ad7dc84
Eugene_dev <lointainyyy@gmail.com>
2023-02-05 20:49:35 +0200

    fix path to img

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -8,18 +8,18 @@ export const links = [
     id: 2,
     title: 'Headphones',
     url: 'headphones',
-    img: '/assets/shared/desktop/image-category-thumbnail-headphones.png',
+    img: '/assets/product-xx99-mark-one-headphones/desktop/image-product.png',
   },
   {
     id: 3,
     title: 'Speakers',
     url: 'speakers',
-    img: '/assets/shared/desktop/image-category-thumbnail-speakers.png',
+    img: '/assets/product-zx9-speaker/desktop/image-product.png',
   },
   {
     id: 4,
     title: 'Earphones',
     url: 'earphones',
-    img: '/assets/shared/desktop/image-category-thumbnail-earphones.png',
+    img: '/assets/product-yx1-earphones/desktop/image-product.png',
   },
 ]
