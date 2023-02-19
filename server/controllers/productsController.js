93d6569b6fb0ad170d2fb5654ef921fc92da5405
Eugene_dev <lointainyyy@gmail.com>
2023-02-19 19:02:27 +0200

    fix logic

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -3,7 +3,7 @@ const Product = require('../models/productsModel')
 const getProducts = async (req, res) => {
 	try {
 		const products = await Product.find(req.query)
-		res.status(200).json(products)
+		products = [] ? res.status(404).json(`no category`) : res.status(200).json(products)
 	} catch (error) {
 		res.status(500).json({ msg: error.message })
 	}
