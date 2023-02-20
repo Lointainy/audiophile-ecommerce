886be85519b4c132a1a0dab59f7ef62d4d10a7c6
Eugene_dev <lointainyyy@gmail.com>
2023-02-20 16:39:11 +0200

    fix logic for media

--- client/src/hooks/useMediaQuery.ts
+++ client/src/hooks/useMediaQuery.ts
@@ -1,9 +1,9 @@
 import { useLayoutEffect, useState } from 'react'
 
 const queries = [
-  { name: 'mobile', query: '(max-width: 767px)', status: false },
-  { name: 'tablet', query: '(min-width: 768px) and (max-width: 991px)', status: false },
-  { name: 'desktop', query: '(min-width: 992px)', status: false },
+  { name: 'mobile', query: '(max-width: 575px)', status: false },
+  { name: 'tablet', query: '(min-width: 576px) and (max-width: 767px)', status: false },
+  { name: 'desktop', query: '(min-width: 768px)', status: false },
 ]
 
 export function useMediaQuery() {
