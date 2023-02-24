2a445411826f2fa62141ed4fe931bbcda2f6b9d0
Eugene_dev <lointainyyy@gmail.com>
2023-02-24 21:47:03 +0200

    fixed logic for theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,21 @@ import router from '@router/router'
 import { RouterProvider } from 'react-router-dom'
 
 /* Store */
-import { useAppSelector } from '@hooks/useRedux'
+import { useAppDispatch, useAppSelector } from '@hooks/useRedux'
+import { setTheme } from './store/reducers/uiSlice'
+import useTheme from './hooks/useTheme'
+import { useEffect } from 'react'
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch()
+
+  const { userTheme } = useTheme()
+
   const theme = useAppSelector((store) => store.ui.colorTheme)
+
+  useEffect(() => {
+    dispatch(setTheme(userTheme))
+  })
 
   return (
     <div className={`app ${theme}`}>
