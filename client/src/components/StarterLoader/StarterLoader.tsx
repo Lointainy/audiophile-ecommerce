eb02fff8f3261d65b301cd0002c0dd3654b7345a
Eugene_dev <lointainyyy@gmail.com>
2023-02-24 21:47:31 +0200

    fixed loader

--- client/src/components/StarterLoader/StarterLoader.tsx
+++ client/src/components/StarterLoader/StarterLoader.tsx
@@ -6,7 +6,7 @@ const StarterLoader: React.FC = () => {
   return (
     <div className={style.overlay}>
       <Loader />
-      <p className={style.desc}>please wait, we are try to connecting to server for get data</p>
+      <p className={style.desc}>please wait, server starts 1 - 2 min for getting data</p>
     </div>
   )
 }
